3ef6a9d9afe2a73c85aa854f7cffb1bf13cc2b46
Richard Pitts <Richardepitts@gmail.com>
2019-09-16 17:45:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here!
+// document.querySelector('main#main').remove();
+
+// var newHeader = document.createElement('h1');
+
+// newHeader.setAttribute('id', 'victory')
+
+// newHeader.innerHTML = '<h1>victory</h1>';
+
+// newHeader.innerText = 'YOUR-NAME is the champion';
+
+// let newHeader = document.createElement('h1');
+// newHeader.id = "victory";
+// document.body.appendChild(newHeader);
+
+// let addingText = document.querySelector("h1#victory")
+// addingText.innerText = "YOUR-NAME is the champion";
+
+// main.remove();
+
+	// Write your code here!
+    // document.querySelector('main').remove();
+    // let newHeader = document.createElement('h1');
+    // newHeader.setAttribute('id', 'victory');
+    // newHeader.innerText = "Myung is the champion";
+
+    document.querySelector("main").remove();
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory"
+newHeader.innerText = "Seann is the champion" 
